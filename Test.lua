e936581218b3ecd3201de870c10f8d8a53381a99
TedMcbur <142521760+Tedmcbur@users.noreply.github.com>
2023-10-24 07:38:16 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,5 +1,6 @@
 Local ted = Workspace:findfirstchild("ted")
 Local tedcolor = notblu
-if tedcolor then
+if tedcolor then --tedcolor grabs the value which is "notblu"
 ted.color = blu --this prob takes hex code lol
 end
+--skidders get stitches
