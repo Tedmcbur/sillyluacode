104b056133f37ef84bc2623e60f81bba2d930be5
TedMcbur <142521760+Tedmcbur@users.noreply.github.com>
2023-10-24 09:09:04 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,3 +4,7 @@ if tedcolor then --tedcolor grabs the value which is "notblu"
 ted.color = blu --this prob takes hex code lol
 end
 --skidders get stitches
+Local codeownership = teddythedev --changing this value activates the print function
+If codeownership ~= teddythedev then
+print("dirty skid lololololololol")
+end
